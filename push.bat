4de33c1500fffa25d4350200bc42e821a84e327f
Henry-chr <850318517@qq.com>
2021-02-20 21:07:05 +0800

    2021/02/20 21:06:59 backup from windows, comments: 测试

--- push.bat
+++ push.bat
@@ -8,7 +8,15 @@ set /p comments=please input commit comments:
 git status
 git add .
 git commit -m "%timestamp% backup from windows, comments: %comments%"
-git push origin main
-echo "Finished Push!"
+git push github main
+echo "Finished Push github!"
+git log --stat -1
+pause
+
+git status
+git add .
+git commit -m "%timestamp% backup from windows, comments: %comments%"
+git push gitee main
+echo "Finished Push gitee!"
 git log --stat -1
 pause
